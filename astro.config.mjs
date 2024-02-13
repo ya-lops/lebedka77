8ee7a59ebc6659a917ed4ca911ddc4b2222c947d
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-02-13 21:16:35 +0300

    tune

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-import { defineConfig } from 'astro/config';
-import alpine from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import alpine from "@astrojs/alpinejs";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +10,16 @@ export default defineConfig({
     assets: "assets",
     inlineStylesheets: "never",
   },
-  integrations: [alpine({ entrypoint: '/src/entrypoint' })],
+  integrations: [
+    alpine({
+      entrypoint: "/src/entrypoint",
+    }),
+    compress({
+      CSS: false,
+      HTML: true,
+      Image: true,
+      JavaScript: true,
+      SVG: false,
+    }),
+  ],
 });
