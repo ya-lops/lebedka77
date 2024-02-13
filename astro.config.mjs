2483f0f77dcef05caadf744fd814200bf3637122
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-02-13 12:00:29 +0300

    init

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import alpine from '@astrojs/alpinejs';
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+  build: {
+    assets: "assets",
+    inlineStylesheets: "never",
+  },
+  integrations: [alpine({ entrypoint: '/src/entrypoint' })],
+});
---
+++ src/data/reviews.js
@@ -0,0 +1,22 @@
+const reviews = [
+  {
+    text: 'Застрял в грязи на своем грузовике, и я не мог выбраться самостоятельно. Позвонил и они приехали очень быстро, вытащили мой грузовик из грязи и сделали это очень профессионально. Я очень благодарен за их помощь!',
+    name: "Иван C.",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/75/759f7cc384896611499df40e7c5c3e5ed4e03e2e_full.jpg",
+  },
+  {
+    text: 'Я застрял в снегу на своем квадроцикле, и я не мог выбраться самостоятельно. Я позвонил в компанию "Лебедка", и они приехали очень быстро. Они вытащили мой квадроцикл из снега и сделали это очень профессионально. Я очень благодарен за их помощь! Еще вступил в группу и получил скидку. Зачет!',
+    name: "Петр П.",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/7e/7e1136c2bad539b7287c65a9fc1179bf91c188b8_full.jpg",
+  },
+  {
+    text: 'Улетела в кювет на своем мурзике, и я не смогла выбраться самостоятельно. Позвонила в лебедку по совету друзей – вытащили быстро. Большое спасибо за их помощь!',
+    name: "Анна Б.",
+    image:
+      "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/ac/acc84a608318e40f1e1cce761f2f99e1eae40be8_full.jpg",
+  },
+];
+
+export default reviews;
