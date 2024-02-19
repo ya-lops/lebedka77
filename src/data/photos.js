c17be7070238c3723130a5b56fbb2d25efbc59d5
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-02-19 12:40:20 +0300

    add photos, slick, jquery

---
+++ src/data/photos.js
@@ -0,0 +1,52 @@
+const photos = [
+  {
+    src: "/photos/lebedka77-19022024-01.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-02.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-03.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-04.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-05.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-06.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-07.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-08.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-09.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-10.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-11.webp",
+    alt: ""
+  },
+  {
+    src: "/photos/lebedka77-19022024-12.webp",
+    alt: ""
+  },
+];
+
+export default photos;
